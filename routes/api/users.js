cad7015db3607c1e75bf91f3ebadc2cdc512b1d7
John <johnmsaylor@gmail.com>
2020-05-25 18:57:27 -0400

    user Model & validation

---
+++ routes/api/users.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
+// @route GET   api/users
+// @description Post user
+// @access      Public
+router.post(
+  "/",
+  [
+    check("name", "Make sure to add your name").not().isEmpty(),
+    check("email", "Please add your email").isEmail(),
+    check("password", "mininum 6 characters for password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "user exists" }] });
+      }
+      // Get users pfp
+      const avatar = await gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      // create a new Mongo User instance
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      // Return JWT
+      res.send("User Created");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("server error");
+    }
+
+    console.log(req.body);
+    res.send("User Route");
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//start server
 app.listen(PORT, () => console.log(`you got it - listening on ${PORT}`));
 
+//connect to Mongo DB
 connectDB();
+
+//Middleware
+app.use(express.json({ extended: false }));
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
