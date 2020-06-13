8d9745935fb0a01f5130d547546229e2bbe47877
John <johnmsaylor@gmail.com>
2020-06-13 12:22:22 -0400

    create/update profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
 // @route GET   api/profile/me
 // @description Get current user's profile
@@ -16,6 +17,7 @@ router.get(
       //grab the profile document from DB
       const profile = await Profile.findOne({
         user: req.user.id,
+        //not exactly sure I follow how Profile populates from User - connected by embed?
       }).populate("user", ["name", "avatar"]);
 
       //behavior if DB doesn't find the DB entry
@@ -24,6 +26,85 @@ router.get(
       }
     } catch (error) {
       //does we only use catch because we are using async?
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route       POST api/profile
+// @description Create or Update profile
+// @access      Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills are required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //pull fields out from request body
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skills) => skills.trim());
+    }
+    //Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        //Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      //Create
+
+      profile = new Profile(profileFields);
+      //is .create() better now? because of locking
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
     }
