bc8942ffef18e157ab91554ecddd43d8afda227c
John <johnmsaylor@gmail.com>
2020-07-18 18:21:23 -0400

    Login and Register frontend

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Login from "./components/auth/Login";
 const App = () => {
   return (
     <Router>
-      <Fragment className='App'>
+      <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
